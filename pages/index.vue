ae52ab46579a143b2df19802cc1af5a487abb023
René Eschke <rene@eschke.info>
2024-03-18 17:42:49 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <pre>{{ data.joke }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+
+const { data } = await useFetch<any>('https://icanhazdadjoke.com', {
+  headers: {
+    Accept: 'application/json',
+  },
+
+  getCachedData(key) {
+    return nuxtApp.payload[key] || nuxtApp.static.data[key];
+  },
+});
+</script>
